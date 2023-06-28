75b544c4d224152ffff9c1455114280c001b7db6
alfa-th <itsalfath123@gmail.com>
2023-06-28 23:25:03 +0700

    added reference in fetch_data.py

--- scripts/fetch_data.py
+++ scripts/fetch_data.py
@@ -1,2 +1,3 @@
 # TODO: utilize kaggle to fetch the movie metadata file
 # REF: https://kevinheavey.github.io/modern-polars/indexing.html
+# REF: https://stackoverflow.com/questions/49386920/download-kaggle-dataset-by-using-python
